d714b073d89ba4105d9949c13bf2feba705dae23
KHALED <khaled.soliman97@gmail.com>
2020-06-28 14:58:31 +0200

    importing time, modified_angles and picamera modules. adding capturing methods and modified_joints functions. fixing bug: indexing the coordinate tuple in get_angles function instead of passing the whole tuple.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
+from time import sleep
+from picamera import PiCamera
 from detection import ObjectDetection
 from gui import user_input
 from inversekinematics import get_angles            # importing kinematics equations
-import joints                                       # importing motors functions
+import modified_joints                              # importing motors functions
 number = 1                                          # initial no. to start with capturing function
 
-
+camera = PiCamera()
+camera.resolution = (1024, 768)
 triangles_check, squares_check, circles_check, red_check, green_check, blue_check = user_input()
 
 
@@ -15,17 +18,20 @@ triangles_check, squares_check, circles_check, red_check, green_check, blue_chec
 if triangles_check or squares_check or circles_check:
     detected_shape = ObjectDetection(np.array([0, 121, 26]), np.array([180, 255, 255]), 2000)
 elif green_check:
-    green_color = ObjectDetection(np.array([30, 28, 0]), np.array([71, 255, 198]), 2000)
+    green_color = ObjectDetection(np.array([65,138,65]), np.array([98,233,193]), 2000)
 elif red_check:
-    red_color = ObjectDetection(np.array([0, 178, 211]), np.array([17, 255, 255]), 2000)
+    red_color = ObjectDetection(np.array([0,90,90]), np.array([180,174,132]), 2000)
 elif blue_check:
-    blue_color = ObjectDetection(np.array([56, 153, 62]), np.array([180, 255, 255]), 2000)
+    blue_color = ObjectDetection(np.array([103,139,27]), np.array([157,220,153]), 2000)
 
 while red_check:
-    # capturing the frame
-    pic = cv2.imread(f"C:\\Users\\KHALED\\Desktop\\pics\\{number}.jpg")
-    # pic = cv2.resize(pic, (int(pic.shape[1] / 2), int(pic.shape[0] / 2)))
-    red_coors, red_mask, red_img = red_color.color(pic)
+    
+    camera.start_preview()
+    sleep(2)
+    camera.capture(f'/home/pi/Desktop/images/img{number}.jpg')
+    camera.stop_preview()
+    pic = cv2.imread(f'/home/pi/Desktop/images/img{number}.jpg')
+    red_coors, red_mask, red_img = red_color.color(img=pic)
     if len(red_coors) == 0:
         red_check = False
         print("There are no red objects left!")
@@ -35,7 +41,7 @@ while red_check:
         cv2.moveWindow("red img", 0,0)
         # cv2.imshow("red mask", red_mask)
         cv2.imshow("red img", red_img)
-        cv2.waitKey(2000)
+        cv2.waitKey(3000)
         cv2.destroyAllWindows()
 
     else:
@@ -46,22 +52,23 @@ while red_check:
         cv2.moveWindow("red img", 0, 0)
         # cv2.imshow("red mask", red_mask)
         cv2.imshow("red img", red_img)
-        cv2.waitKey(4000)
+        cv2.waitKey(2000)
         cv2.destroyAllWindows()
-        number += 1                         # to rename the next captured image
-        angles = get_angles(red_coors[0])
-        joints.move(angles)
-        joints.pump()       # turn on the pump
+        number += 1     # to rename the next captured image
+        modified_joints.move(get_angles(red_coors[0][0], red_coors[0][1], red_coors[0][2]))
+        sleep(2)
+        # joints.pump()       # turn on the pump
         joints.red_drop_pos()
-        joints.pump()       # turn of the pump
-        joints.parking_pos()
+        sleep(2)
+        # joints.pump()       # turn of the pump
+        modified_joints.parking_pos()
 
-
-number = 2  # will be removed
 while green_check:
-    # capturing the frame
-    pic = cv2.imread(f"C:\\Users\\KHALED\\Desktop\\pics\\{number}.jpg")
-    # pic = cv2.resize(pic, (int(pic.shape[1] / 2), int(pic.shape[0] / 2)))
+    camera.start_preview()
+    sleep(2)
+    camera.capture(f'/home/pi/Desktop/images/img{number}.jpg')
+    camera.stop_preview()
+    pic = cv2.imread(f'/home/pi/Desktop/images/img{number}.jpg')
     green_coors, green_mask, green_img = green_color.color(pic)
     if len(green_coors) == 0:
         green_check = False
@@ -83,21 +90,22 @@ while green_check:
         cv2.moveWindow("green img", 0, 0)
         # cv2.imshow("green mask", green_mask)
         cv2.imshow("green img", green_img)
-        cv2.waitKey(4000)
+        cv2.waitKey(2000)
         cv2.destroyAllWindows()
         number += 1                         # to rename the next captured image
-        angles = get_angles(green_coors[0])
-        joints.move(angles)
-        joints.pump()  # turn on the pump
-        joints.green_drop_pos()
-        joints.pump()  # turn of the pump
-        joints.parking_pos()
-
-number = 3  # will be removed
+        modified_joints.move(get_angles(green_coors[0][0], green_coors[0][1], green_coors[0][2]))
+        sleep(2)
+        # joints.pump()  # turn on the pump
+        modified_joints.green_drop_pos()
+        sleep(2)
+        # joints.pump()  # turn of the pump
+        modified_joints.parking_pos()
 while blue_check:
-    # capturing the frame
-    pic = cv2.imread(f"C:\\Users\\KHALED\\Desktop\\pics\\{number}.jpg")
-    # pic = cv2.resize(pic, (int(pic.shape[1] / 2), int(pic.shape[0] / 2)))
+    camera.start_preview()
+    sleep(2)
+    camera.capture(f'/home/pi/Desktop/images/img{number}.jpg')
+    camera.stop_preview()
+    pic = cv2.imread(f'/home/pi/Desktop/images/img{number}.jpg')
     blue_coors, blue_mask, blue_img = blue_color.color(pic)
     if len(blue_coors) == 0:
         blue_check = False
@@ -118,15 +126,16 @@ while blue_check:
         cv2.moveWindow("blue img", 0, 0)
         # cv2.imshow("blue mask", blue_mask)
         cv2.imshow("blue img", blue_img)
-        cv2.waitKey(4000)
+        cv2.waitKey(2000)
         cv2.destroyAllWindows()
         number += 1
-        angles = get_angles(blue_coors[0])
-        joints.move(angles)
-        joints.pump()  # turn on the pump
-        joints.blue_drop_pos()
-        joints.pump()  # turn of the pump
-        joints.parking_pos()
+        modified_joints.move(get_angles(blue_coors[0][0], blue_coors[0][1], blue_coors[0][2]))
+        sleep(2)
+        # joints.pump()  # turn on the pump
+        modified_joints.blue_drop_pos()
+        sleep(2)
+        # joints.pump()  # turn of the pump
+        modified_joints.parking_pos()
 
 number = 1
 while squares_check:
@@ -206,7 +215,7 @@ while circles_check:
         cv2.namedWindow("circles", cv2.WINDOW_AUTOSIZE)
         cv2.moveWindow("circles", 0, 0)
         cv2.imshow("circles", circles_img)
-        cv2.waitKey(4000)
+        cv2.waitKey(2000)
         cv2.destroyAllWindows()
         number += 1
         angles = get_angles(circles_coors[0])
